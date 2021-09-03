18785494dd58ebe7b0c540225d750b52040be809
Francesco <francescococlite@gmail.com>
2021-09-03 15:38:38 +0200

    fine esercizio + bonus

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel primi passi</title>
+    
+    </head>
+    <body>
+        <div>
+            <a href="{{route('homepage')}}">Homepage</a>
+            <a href="{{route('about-us')}}">About us</a>
+            <a href="{{route('link')}}">Link a caso</a>
+            <a href="{{route('banana')}}">Banana</a>
+            <a href="">Non so più cosa scrivere</a>
+        </div>
+    </body>
+</html>
